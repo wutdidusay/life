91b66bf516faaf70d605a03cec1a50cbdbc6a27d
whatdidusay <pikaroc@gmail.com>
2021-12-29 02:01:31 +0900

    map api done

--- common.js
+++ common.js
@@ -5,83 +5,83 @@ document.documentElement.addEventListener('touchstart', function (event) {
   }
 }, false);
 
-$(function(){
+$(function () {
   // noticeMsg = "연금복권이 추가 되었습니다.";
   // $('body').prepend('<p class="notice">' + noticeMsg + '</p>');
   navList = [
     {
-      'name':'랜덤선택',
-      'link':'index'
+      'name': '랜덤선택',
+      'link': 'index'
     },
     {
-      'name':'공사중',
-      'link':'map-api'
+      'name': '근처에서 먹어',
+      'link': 'map-api'
     },
     {
-      'name':'이거 먹어',
-      'link':'eat-this'
+      'name': '이거 먹어',
+      'link': 'eat-this'
     },
     {
-      'name':'술 마셔',
-      'link':'drink'
+      'name': '술 마셔',
+      'link': 'drink'
     },
     {
-      'name':'로또 추첨',
-      'link':'lotto'
+      'name': '로또 추첨',
+      'link': 'lotto'
     },
     {
-      'name':'연금복권 추첨',
-      'link':'pension'
+      'name': '연금복권 추첨',
+      'link': 'pension'
     }
   ];
 
-  function navMenu(){
-    for (i=0; i<navList.length; i++){
-      $('nav ul').append('<li>' + '<a href="' + navList[i].link +'.html' + '">' + navList[i].name + '</a>' + '</li>');
+  function navMenu() {
+    for (i = 0; i < navList.length; i++) {
+      $('nav ul').append('<li>' + '<a href="' + navList[i].link + '.html' + '">' + navList[i].name + '</a>' + '</li>');
     }
-  }navMenu()
+  } navMenu()
 
-  function pathMenu(){
+  function pathMenu() {
     var windowPath = window.location.pathname.split("/").pop()
-    for(i=0; i<$('nav li').length; i++){
-        var pathHref = $('nav li').eq(i).find('a').attr('href')
-        if(windowPath == pathHref){
-          $('nav li').removeClass()
-          $('nav li').eq(i).addClass('active')
-          document.title = navList[i].name
-          $('h1').text(navList[i].name)
-        }
+    for (i = 0; i < $('nav li').length; i++) {
+      var pathHref = $('nav li').eq(i).find('a').attr('href')
+      if (windowPath == pathHref) {
+        $('nav li').removeClass()
+        $('nav li').eq(i).addClass('active')
+        document.title = navList[i].name
+        $('h1').text(navList[i].name)
+      }
     }
   } pathMenu()
 
-  function addMenu(){
+  function addMenu() {
     var val = $('input').val();
     var overlap = false;
 
-    if(val == ''){
+    if (val == '') {
       overflap = true;
     } else {
-      for(i=0; i<menu.length; i++){
-        if( val == menu[i] ){
-            alert('같은 메뉴자나 바보야') 
-            $('input').val('');
-            return true
+      for (i = 0; i < menu.length; i++) {
+        if (val == menu[i]) {
+          alert('같은 메뉴자나 바보야')
+          $('input').val('');
+          return true
         }
       }
 
-      if( overlap !== true ){
-          menu.push(val);
-          $('.menu ul').append('<li>' + val + '<i class="del fas fa-minus-circle"></i>' + '</li>');
-          $('input').val('');
+      if (overlap !== true) {
+        menu.push(val);
+        $('.menu ul').append('<li>' + val + '<i class="del fas fa-minus-circle"></i>' + '</li>');
+        $('input').val('');
       }
     }
     console.log(overlap)
     console.log(menu)
   }
 
-  function result(){
+  function result() {
     var ranNumb = Math.floor(Math.random() * menu.length)
-        console.log(ranNumb)
+    console.log(ranNumb)
     var eatThis = []
     var elTop = $('.menu ul li').eq(ranNumb).offset().top
     var halfPos = $(window).height() / 2 - 45
@@ -101,63 +101,63 @@ $(function(){
   var menu = []
   var eatThis = $.getJSON("./menuList.json", function (data) {
     var winPath = window.location.pathname.split("/").pop().split('.').shift()
-    if(winPath == 'eat-this'){
-      for (i=0; i<eatThis.responseJSON.length; i++){
-        for (j=0; j<eatThis.responseJSON[i].list.length; j++){
-            var val = eatThis.responseJSON[i].list[j]
-            menu.push(val)
-            $('.eat-this-wrap ul').append('<li>' + val + '<i class="del fas fa-minus-circle"></i>' + '</li>');
-            $('.update-wrap ul').append('<li>' + val + '</li>');
+    if (winPath == 'eat-this') {
+      for (i = 0; i < eatThis.responseJSON.length; i++) {
+        for (j = 0; j < eatThis.responseJSON[i].list.length; j++) {
+          var val = eatThis.responseJSON[i].list[j]
+          menu.push(val)
+          $('.eat-this-wrap ul').append('<li>' + val + '<i class="del fas fa-minus-circle"></i>' + '</li>');
+          $('.update-wrap ul').append('<li>' + val + '</li>');
         }
       }
     }
     return data
   });
 
-  $('.eat-this .input-wrap li').on('click',function(){
-      $('.input-wrap li').removeClass('active');
-      $(this).addClass('active');
-      menu = []
-      $('.menu ul li').remove()
+  $('.eat-this .input-wrap li').on('click', function () {
+    $('.input-wrap li').removeClass('active');
+    $(this).addClass('active');
+    menu = []
+    $('.menu ul li').remove()
 
-      if(this.id == 'All'){
-          for (i=0; i<eatThis.responseJSON.length; i++){
-              for (j=0; j<eatThis.responseJSON[i].list.length; j++){
-                  var val = eatThis.responseJSON[i].list[j]
-                  menu.push(val)
-                  $('.menu ul').append('<li>' + val + '<i class="del fas fa-minus-circle"></i>' + '</li>');
-              }
-          }
+    if (this.id == 'All') {
+      for (i = 0; i < eatThis.responseJSON.length; i++) {
+        for (j = 0; j < eatThis.responseJSON[i].list.length; j++) {
+          var val = eatThis.responseJSON[i].list[j]
+          menu.push(val)
+          $('.menu ul').append('<li>' + val + '<i class="del fas fa-minus-circle"></i>' + '</li>');
+        }
       }
+    }
 
-      for (i=0; i<eatThis.responseJSON.length; i++){
-          if( this.id == eatThis.responseJSON[i].food ) {
-              pick = i;
-              for (j=0; j<eatThis.responseJSON[pick].list.length; j++){
-                var val = eatThis.responseJSON[pick].list[j]
-                menu.push(val)
-                $('.menu ul').append('<li>' + val + '<i class="del fas fa-minus-circle"></i>' + '</li>');
-              }
-          }
+    for (i = 0; i < eatThis.responseJSON.length; i++) {
+      if (this.id == eatThis.responseJSON[i].food) {
+        pick = i;
+        for (j = 0; j < eatThis.responseJSON[pick].list.length; j++) {
+          var val = eatThis.responseJSON[pick].list[j]
+          menu.push(val)
+          $('.menu ul').append('<li>' + val + '<i class="del fas fa-minus-circle"></i>' + '</li>');
+        }
       }
-      console.log(menu)
+    }
+    console.log(menu)
   });
 
   // drink page menu load
   var menu = []
   var drinkMenu = $.getJSON("./drinkList.json", function (data) {
     var winPath = window.location.pathname.split("/").pop().split('.').shift()
-    if(winPath == 'drink'){
-      for (i=0; i<drinkMenu.responseJSON.length; i++){
-        for (j=0; j<drinkMenu.responseJSON[i].list.length; j++){
-            var val = drinkMenu.responseJSON[i].list[j]
-            menu.push(val)
+    if (winPath == 'drink') {
+      for (i = 0; i < drinkMenu.responseJSON.length; i++) {
+        for (j = 0; j < drinkMenu.responseJSON[i].list.length; j++) {
+          var val = drinkMenu.responseJSON[i].list[j]
+          menu.push(val)
         }
       }
       var dupMenu = [...new Set(menu)]
       menu = dupMenu
 
-      for (i=0; i<menu.length; i++){
+      for (i = 0; i < menu.length; i++) {
         $('.drink-wrap ul').append('<li>' + menu[i] + '<i class="del fas fa-minus-circle"></i>' + '</li>');
         $('.update-drink-wrap ul').append('<li>' + menu[i] + '</li>');
       }
@@ -165,54 +165,54 @@ $(function(){
     return data
   });
 
-  $('.drink .input-wrap li').on('click',function(){
-      $('.input-wrap li').removeClass('active');
-      $(this).addClass('active');
-      menu = []
-      $('.menu ul li').remove()
+  $('.drink .input-wrap li').on('click', function () {
+    $('.input-wrap li').removeClass('active');
+    $(this).addClass('active');
+    menu = []
+    $('.menu ul li').remove()
 
-      if(this.id == 'All'){
-          for (i=0; i<drinkMenu.responseJSON.length; i++){
-              for (j=0; j<drinkMenu.responseJSON[i].list.length; j++){
-                  var val = drinkMenu.responseJSON[i].list[j]
-                  menu.push(val)
-              }
-          }
-          var dupMenu = [...new Set(menu)]
-          menu = dupMenu
-
-          for (i=0; i<menu.length; i++){
-            $('.menu ul').append('<li>' + menu[i] + '<i class="del fas fa-minus-circle"></i>' + '</li>');
-          }
+    if (this.id == 'All') {
+      for (i = 0; i < drinkMenu.responseJSON.length; i++) {
+        for (j = 0; j < drinkMenu.responseJSON[i].list.length; j++) {
+          var val = drinkMenu.responseJSON[i].list[j]
+          menu.push(val)
+        }
       }
+      var dupMenu = [...new Set(menu)]
+      menu = dupMenu
 
-      for (i=0; i<drinkMenu.responseJSON.length; i++){
-          if( this.id == drinkMenu.responseJSON[i].food ) {
-              pick = i;
-              for (j=0; j<drinkMenu.responseJSON[pick].list.length; j++){
-                var val = drinkMenu.responseJSON[pick].list[j]
-                menu.push(val)
-                $('.menu ul').append('<li>' + val + '<i class="del fas fa-minus-circle"></i>' + '</li>');
-              }
-          }
+      for (i = 0; i < menu.length; i++) {
+        $('.menu ul').append('<li>' + menu[i] + '<i class="del fas fa-minus-circle"></i>' + '</li>');
       }
-      console.log(menu)
+    }
+
+    for (i = 0; i < drinkMenu.responseJSON.length; i++) {
+      if (this.id == drinkMenu.responseJSON[i].food) {
+        pick = i;
+        for (j = 0; j < drinkMenu.responseJSON[pick].list.length; j++) {
+          var val = drinkMenu.responseJSON[pick].list[j]
+          menu.push(val)
+          $('.menu ul').append('<li>' + val + '<i class="del fas fa-minus-circle"></i>' + '</li>');
+        }
+      }
+    }
+    console.log(menu)
   });
 
   // add menu
-  $('.add-menu').click(function(){
+  $('.add-menu').click(function () {
     addMenu()
   })
   $('input').keydown(function (key) {
     if (key.keyCode == 13) {
-        addMenu()
+      addMenu()
     }
   })
 
   // aside navigation menu
-  $('.mark').click(function(){
+  $('.mark').click(function () {
     $('.modal').removeClass('active');
-    if($('nav').is('.active')){
+    if ($('nav').is('.active')) {
       $('nav, .dim').removeClass('active');
     } else {
       $('nav, .dim').addClass('active');
@@ -221,8 +221,8 @@ $(function(){
   });
 
   // menu list modal
-  $('.update-modal').click(function(){
-    if($('.modal').is('.active')){
+  $('.update-modal').click(function () {
+    if ($('.modal').is('.active')) {
       $('.modal, .dim').removeClass('active');
     } else {
       $('.modal, .dim').addClass('active');
@@ -231,58 +231,58 @@ $(function(){
   });
 
   // click dim close all
-  $('.dim').click(function(){
+  $('.dim').click(function () {
     $('nav, .modal, .dim').removeClass('active');
     $('body').removeClass('noscroll')
   });
 
   // all clear
-  $('.clear').click(function(){
+  $('.clear').click(function () {
     menu = []
     $('.menu ul li').remove()
   })
 
   // list clear
-  $('.menu ul').on('click', '.del', function(){
+  $('.menu ul').on('click', '.del', function () {
     var idx = $(this).parent().index()
     menu.splice(idx, 1);
     $(this).parent().addClass('remove')
     setTimeout(() => {
       $(this).parent().remove()
-      }, 200);
+    }, 200);
     console.log(menu)
   })
-    
+
   // show result
-  $('.result').on('click',function(){
+  $('.result').on('click', function () {
     console.log(menu)
     result()
     var el = document.querySelector(".menu li.active")
     setTimeout(() => {
       party.confetti(el, {
         count: party.variation.range(100, 100),
-	      size: party.variation.range(1, 1.5)
+        size: party.variation.range(1, 1.5)
       });
     }, 400);
   })
 
   // scroll to top
-  $('.to-top').click(function(){
+  $('.to-top').click(function () {
     $('html, body').animate({
       scrollTop: 0
     }, 500)
   })
 
   // top button show
-  $(window).scroll(function(){
+  $(window).scroll(function () {
     var sct = $(this).scrollTop();
     // var scb = sct + $(this).height();
     // var scw = $(document).height();
 
     // if(scb >= scw){
-    if(sct >= 100 ){
+    if (sct >= 100) {
       $('.to-top').addClass('active');
-    }else{
+    } else {
       $('.to-top').removeClass('active');
     }
   })
